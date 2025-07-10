6706b90aecb0a4fc5d637a0eadfc0237fc227b58
Syed Gaziul Hoque <syednew5000@gmail.com>
2025-07-11 00:05:30 +0600

    [done] ui update

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function handleScroll() {
                class="h-28 w-28 rounded-full border-4 border-white object-cover transition-all duration-300">
           <div class="name-info flex-1">
             <h1 class="text-name font-bold text-gray-900 mb-1">MD. Tanvirul Islam</h1>
-            <p class="text-gray-600 text-xs mb-2">Creative designer & Content creator</p>
+            <p class="text-gray-600 text-title mb-2">Creative designer & Content creator</p>
             <div class="flex items-center text-gray-500 text-xs">
               <i class="fas fa-map-marker-alt mr-1"></i>
               <span>Bangladesh</span>
